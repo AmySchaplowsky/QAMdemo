a41ed39abea89b662a39638b18445fc04e044c75
Amy Schaplowsky <amy.schaplowsky@gmail.com>
2020-06-27 12:00:32 -0500

    updating indent characters

--- cypress/integration/QAM/TC02_url.spec.js
+++ cypress/integration/QAM/TC02_url.spec.js
@@ -2,7 +2,7 @@ describe("Create and mark-unmark as favorite", () => {
   it("Sign in", () => {
     cy.visit("https://react-redux.realworld.io/#/login");
     cy.title().should("eq", "Conduit");
-    cy.location().
+    cy.location("protocol").should("eq", "https");
     cy.get("input[type='email']").type("amy.schaplowsky@gmail.com");
     cy.get("input[type='password']").type(atob("VG9kYXkxMjMh"));
     cy.get(".btn").contains("Sign in").should("be.visible").click();
