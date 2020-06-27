6a04a605f3a0d12163ff2deaeda7eef8d8a4c47b
Amy Schaplowsky <amy.schaplowsky@gmail.com>
2020-06-27 13:21:56 -0500

    Adding identifying elements commands

--- cypress/integration/QAM/TC03_identifyElements.spec.js
+++ cypress/integration/QAM/TC03_identifyElements.spec.js
@@ -3,32 +3,41 @@ describe("Create and mark-unmark as favorite", () => {
     cy.visit("https://react-redux.realworld.io/#/login");
     cy.title().should("eq", "Conduit");
     cy.location("protocol").should("eq", "https:");
-    cy.get("input[type='email']").type("amy.schaplowsky@gmail.com");
-    cy.get("input[type='password']").type(atob("VG9kYXkxMjMh"));
-    cy.get(".btn").contains("Sign in").should("be.visible").click();
+    // cy.get("input[type='email']").type("amy.schaplowsky@gmail.com");
+    // cy.get("input[type='password']").type(atob("VG9kYXkxMjMh"));
+    // cy.get(".btn").contains("Sign in").should("be.visible").click();
+
+    cy.get("form").within(($form) => {
+      cy.get("input[type='email']").type("amy.schaplowsky@gmail.com");
+      cy.get("input[type='password']").type(atob("VG9kYXkxMjMh"));
+      cy.root().submit(); // submits the form yielded from 'within'
+    });
     cy.contains("Your Feed", { timeout: 10000 }).should("be.visible");
   });
 
   it("Creat a post", () => {
-    cy.contains("New Post").click();
+    // cy.contains("New Post").click();
+    cy.get("ul.navbar-nav").children().contains("New Post").click();
+
     cy.hash().should("include", "#/editor");
     // cy.location("hash").should("include", "#/editor");
-    cy.get("input[placeholder='Article Title']").type("Test");
-    cy.get('input[placeholder="What\'s this article about?"]').type("Test 1");
-    cy.get("textarea[placeholder='Write your article (in markdown)']").type(
-      "Test 2"
-    );
-    cy.contains("Publish Article").click();
+    cy.get("form").within(($form) => {
+      cy.get("input").first().type("Test");
+      cy.get("input").eq(1).type("Test 1");
+      cy.get("textarea").last().type("Test 2");
+      cy.contains("Publish Article").click();
+    });
     cy.url().should("include", "article");
   });
 
   it("Mark-unmark as favorite", () => {
-    cy.get(".nav-link").contains("AmySchaplowsky").click();
+    // cy.get(".nav-link").contains("AmySchaplowsky").click();
+    cy.get("ul.navbar-nav").children().contains("AmySchaplowsky").click();
     cy.contains("My Articles").should("be.visible");
-    cy.get(".ion-heart").click();
+    cy.get(".ion-heart").first().click();
     cy.contains("Favorited Articles").click();
     cy.url().should("include", "favorites");
-    cy.get(".ion-heart").click();
+    cy.get(".ion-heart").first().click();
     cy.reload();
     cy.contains("No articles are here... yet.").should("be.visible");
     cy.go("back");
